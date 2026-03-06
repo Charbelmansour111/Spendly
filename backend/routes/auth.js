da01590a322454a0ece49075dfb65e5289f40f36
Charbel Mansour <charbel.mansourb@gmail.com>
2026-03-06 14:49:45 +0200

    Fix SMTP port to 587

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,9 @@ const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
